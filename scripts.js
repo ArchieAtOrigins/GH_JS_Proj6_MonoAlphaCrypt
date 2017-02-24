0f2d15bc6a995156cc53f064d217ee79ac0d0345
ArchieAtOrigins <archie@originscreative.com>
2017-02-24 10:24:40 -0800

    Now displaying alphabet string and cipher string

--- scripts.js
+++ scripts.js
@@ -1,26 +1,25 @@
-// OK! Now we're returning a scrambled alphabet.
+function createRandStr() {
 
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-
-function createRandStr() {
-
 var randChar = [];
 var newArry = [];
-
+document.getElementById('alphabet').innerHTML = alphabet;
 while (alphabet.length > 0) {
 	randChar = alphabet.splice([Math.random() * alphabet.length],1);
 
 	newArry.push.apply(newArry, randChar);
 
-	document.getElementById('alphabet').innerHTML = "DANGER WILL ROBINSON! DANGER!";
-	document.getElementById('cipher').innerHTML = newArry;
 	
+	document.getElementById('cipher').innerHTML = newArry;
+	}
 
 	console.log(randChar);
 	console.log(newArry);
 	console.log(alphabet);
-	}
+
+	//When I concatenate the array to a string does it change it's type to a string?
+	console.log(newArry + " •••• Am I now a string?")
 }
 
 createRandStr();
