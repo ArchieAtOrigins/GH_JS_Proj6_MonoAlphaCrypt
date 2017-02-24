10d60030fe8eab46e3029c351d94c133a8903345
ArchieAtOrigins <archie@originscreative.com>
2017-02-23 17:02:23 -0800

    Now producing a scrampled alphabet

--- scripts.js
+++ scripts.js
@@ -1,18 +1,28 @@
+// OK! Now we're returning a scrambled alphabet.
+
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 
 function createRandStr() {
 
-var randChar = '';
+var randChar = [];
 var newArry = [];
+
 while (alphabet.length > 0) {
 	randChar = alphabet.splice([Math.random() * alphabet.length],1);
-	newArry.push(randChar);
+
+	newArry.push.apply(newArry, randChar);
+
+	document.getElementById('alphabet').innerHTML = "DANGER WILL ROBINSON! DANGER!";
+	document.getElementById('cipher').innerHTML = newArry;
+	
 
 	console.log(randChar);
 	console.log(newArry);
 	console.log(alphabet);
 	}
-	
 }
 
 createRandStr();
+
+
